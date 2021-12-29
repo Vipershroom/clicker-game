9c852947769720081357b27530720c736512e356
Vipershroom <undertalefan812@gmail.com>
2021-12-29 00:16:31 -0500

    Added variables for the functions later

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ let amountEl = document.getElementById('amount-el')
 let counts = 0
 let increment = 1
 let twoMax = false
+let fourMax = false
+let mrZom = false
+let autoClicker = false
+let sixTimes = false
+let manyTimes = false
 
 function debugMagic() {
     counts += 100000
@@ -24,8 +29,8 @@ function shop() {
 
  function twoTimes() {
      if (counts >= 100 && twoMax == false) {
-         counts = counts - 100
-         increment = increment * 2
+         counts -= 100
+         increment *= 2
          amountEl.textContent = `Amount: ${counts}`
          twoMax = true
      }
